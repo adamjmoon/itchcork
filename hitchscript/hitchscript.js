2f9f00ad8706c99bd7523848c7d8fcd979cb81ae
Adam Moon <amoon@onetechnologies.net>
2013-05-08 18:13:32 -0500

    fix coffee

--- hitchscript/hitchscript.js
+++ hitchscript/hitchscript.js
@@ -3,7 +3,9 @@ if (Meteor.isClient) {
     Template.hitchscript.created = function () {
 
         var testcase = '';
-        if (window.location.hash)
+        if (window.location.pathname && window.location.pathname.length>1)
+            testcase = window.location.pathname.split('/')[1];
+        else if (window.location.hash)
             testcase = window.location.hash.split('#')[1];
         else
             testcase = 'datetime';
@@ -13,20 +15,38 @@ if (Meteor.isClient) {
             paths: {
                 'ItchCork': 'raw.github.com/adamjmoon/itchcork/master/itchcork.min',
                 'ThemeManager': 'raw.github.com/adamjmoon/itchcork/master/itchcork.min',
+                'js2coffee': 'raw.github.com/adamjmoon/itchcork/master/vendor/js2coffee.min.js',
                 'lodash': 'cdnjs.cloudflare.com/ajax/libs/lodash.js/1.2.0/lodash.min',
                 'platform': 'cdnjs.cloudflare.com/ajax/libs/platform/0.4.0/platform.min',
                 'benchmark': 'raw.github.com/bestiejs/benchmark.js/master/benchmark',
                 'knockout': 'ajax.aspnetcdn.com/ajax/knockout/knockout-2.2.1',
                 'context': 'raw.github.com/adamjmoon/itchcork/master/context/' + testcase,
-                'run': 'raw.github.com/adamjmoon/itchcork/master/test/' + testcase
+                'test': 'raw.github.com/adamjmoon/itchcork/master/test/' + testcase
             }
         });
-        require(['run', 'ItchCork', 'knockout', 'platform', 'lodash', 'benchmark'], function (run,ic) {
-
-            var runSpecs = new run(new ic());
+        require(['test', 'ItchCork', 'knockout', 'platform', 'lodash', 'benchmark', 'js2coffee'], function (test, ic) {
+            var runSpecs = new test(new ic());
         });
-    }
-};
+    };
+
+    Template.ga.rendered = function() {
+      if (!(window._gaq != null)) {
+        window._gaq = [];
+        _gaq.push(['_setAccount', 'UA-40741651-1']);
+        _gaq.push(['_trackPageview']);
+        return (function() {
+          var ga, gajs, s;
+          ga = document.createElement('script');
+          ga.type = 'text/javascript';
+          ga.async = true;
+          gajs = '.google-analytics.com/ga.js';
+          ga.src = 'https:' === document.location.protocol ? 'https://ssl' + gajs : 'http://www' + gajs;
+          s = document.getElementsByTagName('script')[0];
+          return s.parentNode.insertBefore(ga, s);
+        })();
+      }
+    };
+}
 
 if (Meteor.isServer) {
     Meteor.startup(function () {
