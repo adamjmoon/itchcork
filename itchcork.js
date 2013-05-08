ba3c9002baaf415cfc962ebba0dd537e0d7a4b0b
Adam Moon <itchcork@gmail.com>
2013-05-08 15:51:18 -0500

    Update itchcork.js

--- itchcork.js
+++ itchcork.js
@@ -210,7 +210,7 @@ define("ThemeManager", [], function() {
   	};
   };
 });
-define(['Suite', 'Test', 'Spy', 'Verify', 'ThemeManager'], function(Suite, Test, Spy, Verify, ThemeManager) {
+define("ItchCork",['Suite', 'Test', 'Spy', 'Verify', 'ThemeManager'], function(Suite, Test, Spy, Verify, ThemeManager) {
   return function ItchCork() {
       ItchCork.prototype.Suite = Suite;
       ItchCork.prototype.Test = Test;
