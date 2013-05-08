6ee70d861838f5c1155c41c484590d3f7ba76bca
Adam Moon <itchcork@gmail.com>
2013-05-08 16:05:44 -0500

    Update array.js

--- test/array.js
+++ test/array.js
@@ -1,4 +1,4 @@
-define('array',['ItchCork', 'context'], function (fs, context) {
+define(['ItchCork', 'context'], function (fs, context) {
     return function () {
         var fs = new fs();
         var suite = new fs.Suite('Fastest method of convert array-like to actual arrays', context);
