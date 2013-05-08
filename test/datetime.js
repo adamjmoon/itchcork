f61381d8e03c0110654e18db044db2a5e60493ee
Adam Moon <itchcork@gmail.com>
2013-05-08 15:52:39 -0500

    Update datetime.js

--- test/datetime.js
+++ test/datetime.js
@@ -1,7 +1,6 @@
-define(['ItchCork', 'context'], function (fs, context) {
-    return function () {
-        var fs = new fs();
-        var suite = new fs.Suite('DateTime tests', context);
+define(['context'], function (context) {
+    return function (itchcork) {
+        var suite = new itchcork.Suite('DateTime tests', context);
         suite.shouldEqual(1)
             .compare(function (c, tc) {
                 return c[tc]();
