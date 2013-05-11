bd68aa8006798ad4b355d7a5835ac600acf963e8
Adam Moon <adamjmoon@gmail.com>
2013-05-11 11:45:33 -0500

    fix main

--- main.js
+++ main.js
@@ -7,22 +7,22 @@ else
     testcase = 'datetime';
 
 requirejs.config({
-    baseUrl: 'http://adamjmoon.github.io/itchcork/',
+    baseUrl: 'http://',
     paths: {
-        'jquery': '//cdnjs.cloudflare.com/ajax/libs/jquery/2.0.0/jquery.min',
-        'history': '//raw.github.com/balupton/history.js/master/scripts/compressed/history',
-        'bootstrap': '//netdna.bootstrapcdn.com/twitter-bootstrap/2.3.1/js/bootstrap.min',
-        'underscore': '//cdnjs.cloudflare.com/ajax/libs/underscore.js/1.4.4/underscore-min',
-        'coffeescript': 'javascripts/coffeescript.min',
-        'js2coffee': 'javascripts/js2coffee',
-        'ItchCork': 'javascripts/itchcork.min',
-        'ThemeManager': 'javascripts/itchcork.min',
-        'lodash': 'javascripts/lodash.min',
-        'platform': 'javascripts/platform.min',
-        'benchmark': 'javascripts/benchmark.min',
+        'jquery': 'cdnjs.cloudflare.com/ajax/libs/jquery/2.0.0/jquery.min',
+        'history': 'raw.github.com/balupton/history.js/master/scripts/compressed/history',
+        'bootstrap': 'netdna.bootstrapcdn.com/twitter-bootstrap/2.3.1/js/bootstrap.min',
+        'underscore': 'cdnjs.cloudflare.com/ajax/libs/underscore.js/1.4.4/underscore-min',
+        'coffeescript': 'adamjmoon.github.io/itchcork/javascripts/coffeescript.min',
+        'js2coffee': 'adamjmoon.github.io/itchcork/javascripts/js2coffee',
+        'ItchCork': 'adamjmoon.github.io/itchcork/javascripts/itchcork.min',
+        'ThemeManager': 'adamjmoon.github.io/itchcork/javascripts/itchcork.min',
+        'lodash': 'adamjmoon.github.io/itchcork/javascripts/lodash.min',
+        'platform': 'adamjmoon.github.io/itchcork/javascripts/platform.min',
+        'benchmark': 'adamjmoon.github.io/itchcork/javascripts/benchmark.min',
         'knockout': 'ajax.aspnetcdn.com/ajax/knockout/knockout-2.2.1',
-        'context': 'context/' + testcase,
-        'test': 'test/' + testcase
+        'context': 'adamjmoon.github.io/itchcork/context/' + testcase,
+        'test': 'adamjmoon.github.io/itchcork/test/' + testcase
     }
 });
 require(['jquery', 'underscore', 'knockout', 'history','bootstrap'], function () {
