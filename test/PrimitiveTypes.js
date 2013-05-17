f455620a58b6b2c966e70f66d27aa2d11fe8d93e
Adam Moon <itchcork@gmail.com>
2013-05-17 10:55:38 -0500

    Update PrimitiveTypes.js

--- test/PrimitiveTypes.js
+++ test/PrimitiveTypes.js
@@ -1,10 +1,12 @@
-define(function() {
-  'use strict';
-  return function context() {
-     this.numberPrimitiveValue = 1;
-     this.stringPrimitiveValue = "string";
-     this.booleanPrimitiveValue = true;
-     this.nullPrimitiveValue = null;
-     this.undefinedPrimiteValue;
-  };
+define(['context'], function (context) {
+    return function (itchcork) {       
+      var suite = new itchcork.Suite('Javascript Primitive Values and Types', context);
+      suite
+       .add("number", function(c) {return typeof c.numberPrimitiveValue;})
+       .add("string", function(c) {return typeof c.numberStringValue;})
+       .add("boolean", function(c) {return typeof c.booleanPrimitiveValue;})
+       .add("null", function(c) {return typeof c.nullPrimitiveValue;})
+       .add("undefined", function(c) {return typeof c.undefinedPrimitiveValue;})       
+       .run();
+ };
 });
