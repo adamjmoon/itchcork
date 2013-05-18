47be0051d1a0d62c07d1ef7aac35b5c55b851ec4
Adam Moon <adamjmoon@gmail.com>
2013-05-18 00:57:11 -0500

    fix

--- test/PrimitiveTypes.js
+++ test/PrimitiveTypes.js
@@ -5,11 +5,14 @@ define(['context'], function (context) {
             .add("number", function (c) {
                 return typeof c.numberPrimitiveValue;
             })
-            .add("number", function (c) {
+            .add("function", function (c) {
                 return typeof Number;
             })
             .add("string", function (c) {
                 return typeof c.stringPrimitiveValue;
+            })
+            .add("function", function (c) {
+                return typeof String;
             })
             .add("boolean", function (c) {
                 return typeof c.booleanPrimitiveValue;
