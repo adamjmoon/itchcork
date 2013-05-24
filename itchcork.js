6b86fe655bc5d30598e7435df668b3210d84d01a
Adam Moon <adamjmoon@gmail.com>
2013-05-23 23:27:50 -0500

    fix

--- itchcork.js
+++ itchcork.js
@@ -267,10 +267,9 @@ define("UnitTestFrameworkManager", [], function () {
 define("ItchCork", ['Suite', 'SuiteView', 'Test', 'Spy', 'Verify', 'UnitTestFrameworkManager'], function (Suite, SuiteView, Test, Spy, Verify, UnitTestFrameworkManager) {
     'use strict';
     var ItchCork = function() {
+
         ItchCork.prototype.Suite = Suite;
         ItchCork.prototype.SuiteView = SuiteView;
-        ItchCork.prototype.SuiteViewModel = SuiteViewModel;
-        ItchCork.prototype.BenchmarkViewModel= BenchmarkViewModel;
         ItchCork.prototype.Test = Test;
         ItchCork.prototype.Spy = Spy;
         ItchCork.prototype.Verify = Verify;
