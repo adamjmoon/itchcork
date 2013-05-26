7970db3b5f7830d701de90a7691957664d4e4668
Adam Moon <adamjmoon@gmail.com>
2013-05-25 20:51:21 -0500

    fix

--- itchcork.js
+++ itchcork.js
@@ -138,11 +138,13 @@ define("SuiteView", ['knockout'], function(ko) {
 
     var view =  function() {
         var self = this;
+        var topNavNeight=45;
         self.suites = new ko.observableArray([]);
+        self.bodyHeight = ko.observable(document.body.scrollHeight + "px");
         self.menu = document.getElementById('menu');
         self.view = document.getElementById('view');
         self.frame = document.getElementById('frame');
-        self.menu.style.height = (window.innerHeight - 48) + "px";
+        self.menu.style.height = (window.innerHeight - topNavNeight) + "px";
         ko.applyBindings(self);
 
         self.add = function(suite){
@@ -160,11 +162,15 @@ define("SuiteView", ['knockout'], function(ko) {
             {
                 self.menu.style.display = 'none';
             } else {
-                self.menu.style.height = document.body.scrollHeight + "px";
+
                 menu.style.display = 'block';
                 window.scrollTo(0,0);
             }
         }
+
+        self.setMenuHeight = function(){
+            self.menu.style.height = document.body.scrollHeight-topNavNeight + "px";
+        };
     };
     return view;
 });
