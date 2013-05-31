357e0752e2d939e70a4ae6c4ceff2527f75fcc37
Adam Moon <itchcork@gmail.com>
2013-05-31 10:13:03 -0500

    Update array.js

--- context/array.js
+++ context/array.js
@@ -1,20 +1,23 @@
 define(function() {
   'use strict';
-  return function context() {
+  var context = function() {
    var my_array = [0,1,2,3,4,5,6,7,8,9];
    var s=0,f=0;
 
+   //private method
    function myMethod(){
         return my_array.length;
     };
 
+   //privileged method
    this.slow = function() {
         for(var i = 0; i < my_array.length; i++){};
         for(var i = 0; i < myMethod(); i++){};
         s=s+1;
         return s;
       };
-
+      
+   //privileged method
    this.fast = function() {
     var length = my_array.length;
     for(var i = 0; i < length; i++){};
@@ -23,5 +26,7 @@ define(function() {
     f=f+1;
     return f;
    };
+   
+   return context;
   };
 });
