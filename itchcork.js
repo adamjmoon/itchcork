3179bb64a889232d5af52e74208ef41ba52f0708
Adam Moon <amoon@onetechnologies.net>
2013-06-04 17:16:33 -0500

    fix

--- itchcork.js
+++ itchcork.js
@@ -1,3 +1,19 @@
+define("BenchmarkViewModel", ['knockout'], function(ko) {
+  var vm =  function() {
+      this.name= ko.observable('');
+      this.expression= ko.observable('');
+      this.hz= ko.observable(0);
+      this.relativateMarginError= ko.observable('');
+      this.timesFaster= ko.observable('pending...');
+      this.slowest= ko.observable(false);
+      this.fastest= ko.observable(false);
+      this.iterationPerSampleCycle= ko.observable(0);
+      this.numAnalysisCycles= ko.observable(0);
+      this.numSampleCycles= ko.observable(0);
+  };
+
+  return vm;
+});
 define("Spy", [], function() {
     "use strict";
 	return function(F) {
