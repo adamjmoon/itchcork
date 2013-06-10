be0e46549202c039d62e3f61df73de892a6e23b0
Adam Moon <itchcork@gmail.com>
2013-06-10 11:16:59 -0400

    Update PrimitiveTypes.js

--- examples/context/PrimitiveTypes.js
+++ examples/context/PrimitiveTypes.js
@@ -1,11 +1,11 @@
 define(function() {
   'use strict';
-  var context = function() {
+  var c = function context() {
      this.numberPrimitiveValue = 1;
      this.stringPrimitiveValue = "string";
      this.booleanPrimitiveValue = true;
      this.nullPrimitiveValue = null;
      this.undefinedPrimiteValue;
   };
-  return context;
+  return c;
 });
