bf2fea9de5e0517afc8e8266749072799be23b8b
Adam Moon <adamjmoon@gmail.com>
2013-06-13 22:49:19 -0500

    fix

--- examples/test/PrimitiveTypes.js
+++ examples/test/PrimitiveTypes.js
@@ -1,41 +1,40 @@
-define(['context'], function (context) {
-    var test = function (itchcork) {
-        var suite = new itchcork.Suite('Javascript Primitive Values and Types', context);
-        suite
-            .add("number", function (c) {
-                return typeof c.numberPrimitiveValue;
-            })
-            .add("function", function (c) {
-                return typeof Number;
-            })
-            .add("string", function (c) {
-                return typeof c.stringPrimitiveValue;
-            })
-            .add("function", function (c) {
-                return typeof String;
-            })
-            .add("boolean", function (c) {
-                return typeof c.booleanPrimitiveValue;
-            })
-            .add("number", function (c) {
-                return typeof c.numberPrimitiveValue;
-            })
-            .add("string", function (c) {
-                return typeof c.stringPrimitiveValue;
-            })
-            .add("boolean", function (c) {
-                return typeof c.booleanPrimitiveValue;
-            })
-            .add("object", function (c) {
-                return typeof c.nullPrimitiveValue;
-            })
-            .add("undefined", function (c) {
-                return typeof c.undefinedPrimitiveValue;
-            })
-            .add("undefined", function (c) {
-                return typeof c.notDefined;
-            })
-            .run();
-    };
-    return test;
+define(['context', 'ItchCork'], function (c, ic) {
+
+    var suite = new ic.Suite('Javascript Primitive Values and Types', c);
+    suite.add("number", function (c) {
+            return typeof c.numberPrimitiveValue;
+        })
+        .add("function", function (c) {
+            return typeof Number;
+        })
+        .add("string", function (c) {
+            return typeof c.stringPrimitiveValue;
+        })
+        .add("function", function (c) {
+            return typeof String;
+        })
+        .add("boolean", function (c) {
+            return typeof c.booleanPrimitiveValue;
+        })
+        .add("number", function (c) {
+            return typeof c.numberPrimitiveValue;
+        })
+        .add("string", function (c) {
+            return typeof c.stringPrimitiveValue;
+        })
+        .add("boolean", function (c) {
+            return typeof c.booleanPrimitiveValue;
+        })
+        .add("object", function (c) {
+            return typeof c.nullPrimitiveValue;
+        })
+        .add("undefined", function (c) {
+            return typeof c.undefinedPrimitiveValue;
+        })
+        .add("undefined", function (c) {
+            return typeof c.notDefined;
+        })
+        .run();
+
+    return suite;
 });
