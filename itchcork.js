cab2572192b054f02345a16745d9c7cd091b7300
Adam Moon <amoon@onetechnologies.net>
2013-06-14 10:40:12 -0500

    fix

--- itchcork.js
+++ itchcork.js
@@ -400,6 +400,50 @@ define("Suite", ['Test', 'benchmark', 'knockout','SuiteViewModel','BenchmarkView
     return suite;
 });
 
+define("SuiteView", ['knockout'], function(ko) {
+
+    var view =  function() {
+        var self = this;
+        self.suites = new ko.observableArray([]);
+        self.menu = document.getElementById('menu');
+        self.view = document.getElementById('view');
+        self.setMenuHeight = function(){
+            self.menu.style.height = document.body.scrollHeight + "px";
+        };
+        ko.applyBindings(self);
+
+
+        self.add = function(suite){
+           self.suites.push(suite);
+            suite.vm.benchmarksDone.subscribe(function(newValue) {
+                self.setMenuHeight();
+            });
+        };
+
+        self.setTheme = function(theme){
+            window.ThemeManager.set(theme);
+            $('#logo').click();
+        };
+
+        self.toggleMenu = function(){
+            var menu = document.getElementById('menu');
+            if(self.menu.style.display == 'block')
+            {
+                self.menu.style.display = 'none';
+            } else {
+                self.setMenuHeight();
+                menu.style.display = 'block';
+                window.scrollTo(0,0);
+            }
+        };
+
+
+
+
+    };
+    return view;
+});
+
 define("SuiteViewModel", ['knockout', 'UnitTestFrameworkManager'], function(ko, utfm) {
   var vm =  function() {
       this.suiteDesc = ko.observable('');
