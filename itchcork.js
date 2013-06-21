163788b3ace842542ba8aa4ad093bbca39a1bbff
Adam Moon <amoon@onetechnologies.net>
2013-06-21 16:46:43 -0500

    fix

--- itchcork.js
+++ itchcork.js
@@ -531,7 +531,7 @@ define("Suite", ['Test', 'benchmark','SuiteViewModel', 'BenchmarkViewModel'], fu
     return suite;
 });
 
-define("SuiteView", ['UnitTestFrameworkManager'], function(utfm) {
+define("SuiteView", ['UnitTestFrameworkManager'], function (utfm) {
     function view() {
 
         var self = this;
@@ -544,48 +544,48 @@ define("SuiteView", ['UnitTestFrameworkManager'], function(utfm) {
         self.totalPassed = new ko.observable(0);
         self.totalFailed = new ko.observable(0);
         self.githubAccount = new ko.observable('adamjmoon');
-        self.githubRepo =  new ko.observable('itchcork');
-        self.githubBranch =  new ko.observable('master');
+        self.githubRepo = new ko.observable('itchcork');
+        self.githubBranch = new ko.observable('master');
         self.contextRoot = new ko.observable('raw.github.com/' + self.githubAccount() + '/' + self.githubRepo() + '/' + self.githubBranch() + '/');
         self.vendorRoot = new ko.observable(self.contextRoot() + 'vendor/');
-        self.setMenuHeight = function(){
+        self.setMenuHeight = function () {
             self.menu.style.height = document.body.scrollHeight - 45 + "px";
         };
 
-        self.add = function(suite){
-           suite.num = self.suites().length + 1;
-           self.totalTests(self.totalTests() + suite.vm.tests().length);
-           self.totalPassed(self.totalPassed() + suite.passedCount);
-           self.totalFailed(self.totalFailed() + suite.failedCount);
-           self.suites.push(suite);
-            suite.vm.benchmarksDone.subscribe(function(newValue) {
+        self.add = function (suite) {
+            suite.num = self.suites().length + 1;
+            self.totalTests(self.totalTests() + suite.vm.tests().length);
+            self.totalPassed(self.totalPassed() + suite.passedCount);
+            self.totalFailed(self.totalFailed() + suite.failedCount);
+            self.suites.push(suite);
+            suite.vm.benchmarksDone.subscribe(function (newValue) {
                 self.setMenuHeight();
             });
         };
 
-        self.show = function(){
-           ko.applyBindings(self);
+        self.show = function () {
+            ko.applyBindings(self);
         };
 
-        self.setTheme = function(theme){
+        self.setTheme = function (theme) {
             window.ThemeManager.set(theme);
             $('#logo').click();
         };
 
-        self.toggleMenu = function(){
+        self.toggleMenu = function () {
             var menu = document.getElementById('menu');
-            if(self.menu.style.display == 'block')
-            {
+            if (self.menu.style.display == 'block') {
                 self.menu.style.display = 'none';
             } else {
                 self.setMenuHeight();
                 menu.style.display = 'block';
-                window.scrollTo(0,0);
+                window.scrollTo(0, 0);
             }
         };
 
-        self.setupNiceScroll = function(){
-
+        self.setupNiceScroll = function () {
+            $("div#frame").niceScroll();
+            $('div#frame').getNiceScroll().resize();
         };
     };
     return view;
@@ -735,7 +735,7 @@ require(['https://ajax.aspnetcdn.com/ajax/knockout/knockout-2.2.1.js', 'https://
                         if (window.suiteView.unitTestFrameworkManager.init() === "itchcork") {
                             require(['suite'], function () {
                                 window.suiteView.show();
-
+    
                             });
                         }
                         else {
