b89b54f884b4c6b00f2b3a27af5266ef8a12b362
Adam Moon <amoon@onetechnologies.net>
2013-06-21 18:32:14 -0500

    fix

--- itchcork.js
+++ itchcork.js
@@ -592,7 +592,7 @@ define("SuiteView", ['UnitTestFrameworkManager'], function (utfm) {
             $('html').getNiceScroll().resize();
             $("html").niceScroll();
             console.log(num);
-            window.scrollTo(0, $("[data-target='.suite" + num + "']")[0].position().top);
+            window.scrollTo(0, $("[data-target='.suite" + num + "']").position().top);
         };
         self.collapseAll = function () {
             if ($('div.collapsed').length == self.suites().length) {
@@ -747,7 +747,7 @@ require(['https://ajax.aspnetcdn.com/ajax/knockout/knockout-2.2.1.js', 'https://
 
                     require(['ItchCork', 'context'], function (itchcork) {
                         if (window.suiteView.unitTestFrameworkManager.init() === "itchcork") {
-                            require(['suite'], function () { 
+                            require(['suite'], function () {
                                 window.suiteView.show();
                             });
                         }
