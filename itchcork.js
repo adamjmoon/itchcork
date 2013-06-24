e19befc940dedd4fa6151ef20b67d1e9ab54a185
Adam Moon <amoon@onetechnologies.net>
2013-06-24 18:42:09 -0500

    fix

--- itchcork.js
+++ itchcork.js
@@ -783,7 +783,7 @@ require(['https://ajax.aspnetcdn.com/ajax/knockout/knockout-2.2.1.js', 'https://
                                     _.each(mocha.suite.suites,
                                         function (s) {
                                             console.log(s);
-                                            var suite = new itchcork.Suite(s.title, s.ctx.constructor);
+                                            var suite = new itchcork.Suite(s.title, s.ctx.constructor); 
                                             window.suiteView.add(suite);
                                         });
 
