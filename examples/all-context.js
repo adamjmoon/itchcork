f79ce9175752f8748e20cecfa5ce3cf6ae5ec13a
Adam Moon <amoon@onetechnologies.net>
2013-06-24 11:30:21 -0500

    fix

--- examples/all-context.js
+++ examples/all-context.js
@@ -6,66 +6,6 @@ define('arrayMocha',function() {
   return context;
 });
 
-define('array',function() {
-  'use strict';
-   function context() {
-   var my_array = [0,1,2,3,4,5,6,7,8,9];
-
-
-   //private method
-   function myMethod(){
-        return my_array.length;
-    };
-
-   //privileged method
-   this.slow = function() {
-       var s=0;
-        for(var i = 0; i < my_array.length; i++){};
-        for(var i = 0; i < myMethod(); i++){};
-        s=s+1;
-        return s;
-      };
-
-   //privileged method
-   this.fast = function() {
-    var f=0;
-    var length = my_array.length;
-    for(var i = 0; i < length; i++){};
-        var length = myMethod();
-    for(var i = 0; i < length; i++){};
-        f=f+1;
-    return f;
-   };
-  }
-  return context;
-});
-
-define('datetime',function() {
-     'use strict';
-     function context() {
-         this.currentDateTime1 =
-         function() {
-             var time = new Date().getTime();
-             var date = new Date(time);
-             return date.toString('MM/dd/yyyy');
-         };
-
-         this.currentDateTime2 =
-         function() {
-             return new Date().toString('MM/dd/yyyy');
-         };
-         this.dateFromMilliseconds = function(){
-             return new Date(Date(('/Date(1366831535554)/').replace(/\/Date\((.*?)\)\//gi, "$1")).toString()).toLocaleString();
-         };
-
-         this.currentDateMillesondsFrom_Midnight_JUNE_1_1970 =
-         function() {
-             return new Date().getTime().toString();
-         };
-     }
-    return context;
-});
-
 define('objectcreate',function() {
   'use strict';
    function context() {
@@ -128,4 +68,64 @@ define('primitivetypes',function() {
      this.undefinedPrimiteValue;
   }
   return context;
+});
+
+define('array',function() {
+  'use strict';
+   function context() {
+   var my_array = [0,1,2,3,4,5,6,7,8,9];
+
+
+   //private method
+   function myMethod(){
+        return my_array.length;
+    };
+
+   //privileged method
+   this.slow = function() {
+       var s=0;
+        for(var i = 0; i < my_array.length; i++){};
+        for(var i = 0; i < myMethod(); i++){};
+        s=s+1;
+        return s;
+      };
+
+   //privileged method
+   this.fast = function() {
+    var f=0;
+    var length = my_array.length;
+    for(var i = 0; i < length; i++){};
+        var length = myMethod();
+    for(var i = 0; i < length; i++){};
+        f=f+1;
+    return f;
+   };
+  }
+  return context;
+});
+
+define('datetime',function() {
+     'use strict';
+     function context() {
+         this.currentDateTime1 =
+         function() {
+             var time = new Date().getTime();
+             var date = new Date(time);
+             return date.toString('MM/dd/yyyy');
+         };
+
+         this.currentDateTime2 =
+         function() {
+             return new Date().toString('MM/dd/yyyy');
+         };
+         this.dateFromMilliseconds = function(){
+             return new Date(Date(('/Date(1366831535554)/').replace(/\/Date\((.*?)\)\//gi, "$1")).toString()).toLocaleString();
+         };
+
+         this.currentDateMillesondsFrom_Midnight_JUNE_1_1970 =
+         function() {
+             return new Date().getTime().toString();
+         };
+     }
+    return context;
 });
