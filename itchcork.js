52b49e75cf80048b6f0ae75a95da6deeded792c5
Adam Moon <amoon@onetechnologies.net>
2013-06-24 14:59:40 -0500

    fix

--- itchcork.js
+++ itchcork.js
@@ -575,7 +575,6 @@ define("SuiteView", ['UnitTestFrameworkManager'], function (utfm) {
             suite.vm.benchmarksDone.subscribe(function (newValue) {
                 self.setMenuHeight();
             });
-            self.setupNiceScroll();
         };
 
         self.show = function () {
@@ -739,7 +738,7 @@ require(['https://ajax.aspnetcdn.com/ajax/knockout/knockout-2.2.1.js', 'https://
         requirejs.config({
             baseUrl: 'https://',
             paths: {
-                'bootstrap': 'netdna.bootstrapcdn.com/twitter-bootstrap/2.3.1/js/bootstrap.min', 
+                'bootstrap': 'netdna.bootstrapcdn.com/twitter-bootstrap/2.3.1/js/bootstrap.min',
                 'coffeescript': suiteView.vendorRoot() + 'coffee/coffeescript.min',
                 'js2coffee': suiteView.vendorRoot() + 'coffee/js2coffee',
                 'lodash': suiteView.vendorRoot() + 'aa.lodash.min',
