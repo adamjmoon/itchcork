079ed4fba77c7ecce80cb1b8f13d705e3da6aa3b
Adam Moon <amoon@onetechnologies.net>
2013-06-24 11:55:33 -0500

    fix

--- itchcork.js
+++ itchcork.js
@@ -399,7 +399,7 @@ define("Suite", ['Test', 'benchmark', 'SuiteViewModel', 'BenchmarkViewModel'], f
                 .replace(/\/\/(.*)/gm, '<span class="comment">//$1</span>')
                 .replace(/('.*?')/gm, '<span class="string">$1</span>')
                 .replace(/\bnew *(\w+)/gm, '<span class="keyword">new</span> <span class="init">$1</span>')
-                .replace(/\b(function|new|throw|return|var|if|else)\b/gm, '<span class="keyword">$1</span>')
+                .replace(/\b(function|new|throw|return|var|if|else|prototype|Object|->|@|::|this)\b/gm, '<span class="keyword">$1</span>')
         };
         self.setupContextBreakdown = function (context, base) {
             var jsStr = '', coffeeStr = '';
@@ -741,7 +741,7 @@ require(['https://ajax.aspnetcdn.com/ajax/knockout/knockout-2.2.1.js', 'https://
                 'platform': suiteView.vendorRoot() +'platform',
                 'benchmark': suiteView.vendorRoot() +'benchmark',
                 'context': suiteView.contextRoot() + 'examples/all-context',
-                'suite': suiteView.contextRoot() + 'examples/test/' + suite
+                'suite': suiteView.contextRoot() + 'examples/test/' + suite 
             }
         });
         require(['bootstrap', 'sinon'], function () {
