146d0a6067b57c6f737e04ff2820a6ba08117ed9
Adam Moon <amoon@onetechnologies.net>
2013-06-25 17:13:56 -0500

    fix

--- examples/all-context.js
+++ examples/all-context.js
@@ -3,7 +3,7 @@ define('arrayMocha',function() {
     self = this;
     this.arr = [1, 2, 3];
     this.fun = function fun(){
-        self.arr = _.map(self.arr, function(num){ return num * 3; });_
+        self.arr = _.map(self.arr, function(num){ return num * 3; });
     }
   }
   return context;
