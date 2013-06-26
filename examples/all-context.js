5c22633b091ed90487f20bfd13940f61b0e35492
Adam Moon <adamjmoon@gmail.com>
2013-06-25 22:47:38 -0500

    fix

--- examples/all-context.js
+++ examples/all-context.js
@@ -61,18 +61,6 @@ define('objectcreate',function() {
   return context;
 });
 
-define('primitivetypes',function() {
-  'use strict';
-   function context() {
-     this.numberPrimitiveValue = 1;
-     this.stringPrimitiveValue = "string";
-     this.booleanPrimitiveValue = true;
-     this.nullPrimitiveValue = null;
-     this.undefinedPrimiteValue;
-  }
-  return context;
-});
-
 define('array',function() {
   'use strict';
    function context() {
@@ -131,4 +119,16 @@ define('datetime',function() {
          };
      }
     return context;
+});
+
+define('primitivetypes',function() {
+  'use strict';
+   function context() {
+     this.numberPrimitiveValue = 1;
+     this.stringPrimitiveValue = "string";
+     this.booleanPrimitiveValue = true;
+     this.nullPrimitiveValue = null;
+     this.undefinedPrimiteValue;
+  }
+  return context;
 });
