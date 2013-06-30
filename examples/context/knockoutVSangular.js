07634789aa97f7c776a4c388449e495ee2ee5b55
Adam Moon <adamjmoon@gmail.com>
2013-06-30 18:24:18 -0500

    fix

--- examples/context/knockoutVSangular.js
+++ examples/context/knockoutVSangular.js
@@ -99,7 +99,7 @@ define('knockoutVSangular',function() {
         self.KOData.subscribe(function(){
             KOUpdates10(KOUpdates10() + 1);
         });
-        var KOviewmodel = {data: KOData, updates1: KOUpdates1, updates2: KOUpdates2, updates3: KOUpdates3, updates4: KOUpdates4, updates5: KOUpdates5, updates6: KOUpdates6, updates7: KOUpdates7, updates8: KOUpdates8, updates9: KOUpdates9, updates10: KOUpdates10};
+        var KOviewmodel = {data: self.KOData, updates1: KOUpdates1, updates2: KOUpdates2, updates3: KOUpdates3, updates4: KOUpdates4, updates5: KOUpdates5, updates6: KOUpdates6, updates7: KOUpdates7, updates8: KOUpdates8, updates9: KOUpdates9, updates10: KOUpdates10};
 
         ko.applyBindings(KOviewmodel, document.getElementById('koapp'));
         self.KOclear = function (){
