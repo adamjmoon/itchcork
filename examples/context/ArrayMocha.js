9f36609525b84bbe61590dec5ab34ce5447413c2
Adam Moon <adamjmoon@gmail.com>
2013-06-29 20:07:39 -0500

    fix

--- examples/context/ArrayMocha.js
+++ examples/context/ArrayMocha.js
@@ -1,10 +1,10 @@
 define('arrayMocha',function() {
-  function context() {
+  var context = function context() {
     self = this;
     this.arr = [1, 2, 3];
     this.fun = function fun(){
         self.arr = _.map(self.arr, function(num){ return num * 3; });
     }
-  }
+  };
   return context;
 });
