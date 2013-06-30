719095d50b2fc2e1f84deadc332e17f410fe8deb
Adam Moon <adamjmoon@gmail.com>
2013-06-30 18:36:17 -0500

    fix

--- examples/test/itchcork/knockoutVSangular.js
+++ examples/test/itchcork/knockoutVSangular.js
@@ -1,55 +1,55 @@
 require(['knockoutVSangular', 'ItchCork'], function (c, ic) {
     var suite = new ic.Suite('knockout VS angular', c);
-    suite.add("Angular 10","", function (c) {
+    suite.add("", function (c) {
             c.ANGclear();
             for (var i = 0; i < 10; i++) {
                 c.ANGpush();
             }
             return c.ang_scope.data;
         })
-        .add("Knockout 10","", function (c) {
+        .add("", function (c) {
             c.KOclear();
             for (var i = 0; i < 10; i++) {
                 c.KOpush();
             }
             return c.KOData();
         })
-        .add("Angular 100","", function (c) {
+        .add("", function (c) {
             c.ANGclear();
             for (var i = 0; i < 100; i++) {
                 c.ANGpush();
             }
             return c.ang_scope.data;
         })
-        .add("Knockout 100","", function (c) {
+        .add("", function (c) {
             c.KOclear();
             for (var i = 0; i < 100; i++) {
                 c.KOpush();
             }
             return c.KOData();
         })
-        .add("Angular 500","", function (c) {
+        .add("", function (c) {
             c.ANGclear();
             for (var i = 0; i < 500; i++) {
                 c.ANGpush();
             }
             return c.ang_scope.data;
         })
-        .add("Knockout 500","", function (c) {
+        .add("", function (c) {
             c.KOclear();
             for (var i = 0; i < 500; i++) {
                 c.KOpush();
             }
             return c.KOData();
         })
-        .add("Angular 1000","", function (c) {
+        .add("", function (c) {
             c.ANGclear();
             for (var i = 0; i < 1000; i++) {
                 c.ANGpush();
             }
             return c.ang_scope.data;
         })
-        .add("Knockout 1000","", function (c) {
+        .add("", function (c) {
             c.KOclear();
             for (var i = 0; i < 1000; i++) {
                 c.KOpush();
