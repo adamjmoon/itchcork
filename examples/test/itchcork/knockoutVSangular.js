9031365ea0d653767e5042b4dba53e6ccc865a2c
Adam Moon <adamjmoon@gmail.com>
2013-06-30 20:49:19 -0500

    fix

--- examples/test/itchcork/knockoutVSangular.js
+++ examples/test/itchcork/knockoutVSangular.js
@@ -5,56 +5,56 @@ require(['knockoutVSangular', 'ItchCork'], function (c, ic) {
             for (var i = 0; i < 10; i++) {
                 c.ANGpush();
             }
-            return c.ang_scope.data;
+            return c.ang_scope.data.length;
         })
         .add("", function (c) {
             c.KOclear();
             for (var i = 0; i < 10; i++) {
                 c.KOpush();
             }
-            return c.KOData();
+            return c.KOData().length;
         })
         .add("", function (c) {
             c.ANGclear();
             for (var i = 0; i < 100; i++) {
                 c.ANGpush();
             }
-            return c.ang_scope.data;
+            return c.ang_scope.data.length;
         })
         .add("", function (c) {
             c.KOclear();
             for (var i = 0; i < 100; i++) {
                 c.KOpush();
             }
-            return c.KOData();
+            return c.KOData().length;
         })
         .add("", function (c) {
             c.ANGclear();
             for (var i = 0; i < 500; i++) {
                 c.ANGpush();
             }
-            return c.ang_scope.data;
+            return c.ang_scope.data.length;
         })
         .add("", function (c) {
             c.KOclear();
             for (var i = 0; i < 500; i++) {
                 c.KOpush();
             }
-            return c.KOData();
+            return c.KOData().length;
         })
         .add("", function (c) {
             c.ANGclear();
             for (var i = 0; i < 1000; i++) {
                 c.ANGpush();
             }
-            return c.ang_scope.data;
+            return c.ang_scope.data.length;
         })
         .add("", function (c) {
             c.KOclear();
             for (var i = 0; i < 1000; i++) {
                 c.KOpush();
             }
-            return c.KOData();
+            return c.KOData().length;
         })
         .run();
 
