44b336c26c62c2a4c89e3ad3068a43bba6aecabc
Adam Moon <adamjmoon@gmail.com>
2013-06-30 20:50:54 -0500

    fix

--- examples/test/itchcork/knockoutVSangular.js
+++ examples/test/itchcork/knockoutVSangular.js
@@ -1,55 +1,55 @@
 require(['knockoutVSangular', 'ItchCork'], function (c, ic) {
     var suite = new ic.Suite('knockout VS angular', c);
-    suite.add("", function (c) {
+    suite.add(10, function (c) {
             c.ANGclear();
             for (var i = 0; i < 10; i++) {
                 c.ANGpush();
             }
             return c.ang_scope.data.length;
         })
-        .add("", function (c) {
+        .add(10, function (c) {
             c.KOclear();
             for (var i = 0; i < 10; i++) {
                 c.KOpush();
             }
             return c.KOData().length;
         })
-        .add("", function (c) {
+        .add(100, function (c) {
             c.ANGclear();
             for (var i = 0; i < 100; i++) {
                 c.ANGpush();
             }
             return c.ang_scope.data.length;
         })
-        .add("", function (c) {
+        .add(100, function (c) {
             c.KOclear();
             for (var i = 0; i < 100; i++) {
                 c.KOpush();
             }
             return c.KOData().length;
         })
-        .add("", function (c) {
+        .add(500, function (c) {
             c.ANGclear();
             for (var i = 0; i < 500; i++) {
                 c.ANGpush();
             }
             return c.ang_scope.data.length;
         })
-        .add("", function (c) {
+        .add(500, function (c) {
             c.KOclear();
             for (var i = 0; i < 500; i++) {
                 c.KOpush();
             }
             return c.KOData().length;
         })
-        .add("", function (c) {
+        .add(1000, function (c) {
             c.ANGclear();
             for (var i = 0; i < 1000; i++) {
                 c.ANGpush();
             }
             return c.ang_scope.data.length;
         })
-        .add("", function (c) {
+        .add(1000, function (c) {
             c.KOclear();
             for (var i = 0; i < 1000; i++) {
                 c.KOpush();
