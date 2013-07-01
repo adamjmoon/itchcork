4f61f8614e79bb4b1d699bb249a6d4321df9841d
Adam Moon <adamjmoon@gmail.com>
2013-06-30 20:14:04 -0500

    fix

--- examples/context/knockoutVSangular.js
+++ examples/context/knockoutVSangular.js
@@ -54,7 +54,7 @@ define('knockoutVSangular', function () {
         };
         self.ANGpush = function () {
             self.ang_scope.data += "OMGOMGOMG";
-            self.ang_scope.$apply();
+            //self.ang_scope.$apply();
         };
 
         self.KOData = ko.observable("");
@@ -100,7 +100,7 @@ define('knockoutVSangular', function () {
         });
         var KOviewmodel = {data: self.KOData, updates1: KOUpdates1, updates2: KOUpdates2, updates3: KOUpdates3, updates4: KOUpdates4, updates5: KOUpdates5, updates6: KOUpdates6, updates7: KOUpdates7, updates8: KOUpdates8, updates9: KOUpdates9, updates10: KOUpdates10};
 
-        //ko.applyBindings(KOviewmodel, document.getElementById('koapp'));
+        ko.applyBindings(KOviewmodel, document.getElementById('koapp'));
         self.KOclear = function () {
             self.KOData("");
         };
