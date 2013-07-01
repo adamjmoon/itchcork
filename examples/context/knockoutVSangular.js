9c7de017dd76e975f018b0af9f4a6f9d8e1f7edd
Adam Moon <adamjmoon@gmail.com>
2013-06-30 20:38:58 -0500

    fix

--- examples/context/knockoutVSangular.js
+++ examples/context/knockoutVSangular.js
@@ -54,7 +54,7 @@ define('knockoutVSangular', function () {
         };
         self.ANGpush = function () {
             self.ang_scope.data += "o";
-            self.ang_scope.$apply();
+           // self.ang_scope.$apply();
         };
 
         self.KOData = ko.observable("");
