ad2cbb87164aa2659e1645774f570b7bb8090fb8
Adam Moon <itchcork@gmail.com>
2013-07-23 12:50:35 -0500

    Update all-context.js

--- examples/all-context.js
+++ examples/all-context.js
@@ -136,7 +136,7 @@ define('knockoutBenchmarks', function () {
             KOUpdates2(KOUpdates2() + 1);
         });
         
-        var KOviewmodel = {data: self.KOData, updates1: KOUpdates1, updates2: KOUpdates2, updates3: KOUpdates3, updates4: KOUpdates4, updates5: KOUpdates5, updates6: KOUpdates6, updates7: KOUpdates7, updates8: KOUpdates8, updates9: KOUpdates9, updates10: KOUpdates10};
+        var KOviewmodel = {data: self.KOData, updates1: KOUpdates1, updates2: KOUpdates2};
 
         self.KOclear = function () {
             self.KOData("");
@@ -149,6 +149,7 @@ define('knockoutBenchmarks', function () {
 
     return context;
 });
+
 
 define('primitivetypes',function() {
   'use strict';
