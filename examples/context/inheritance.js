922d8051c21c95cd205c551cd2d1f47cb61a550c
Adam Moon <itchcork@gmail.com>
2013-07-23 17:49:40 -0500

    Update inheritance.js

--- examples/context/inheritance.js
+++ examples/context/inheritance.js
@@ -1,27 +1,27 @@
 define("inheritance", function(){
   function context(){
   
-    this.Mammal = function Mammal(name){ 
+    function Mammal(name){ 
       this.name=name;
       this.offspring=[];
     };
-    this.Mammal.prototype.haveABaby=function haveABaby(){ 
-    	var newBaby=new this.constructor("Baby "+this.name);
-    	this.offspring.push(newBaby);
+    Mammal.prototype.haveABaby=function haveABaby(){ 
+      var newBaby=new this.constructor("Baby "+this.name);
+      this.offspring.push(newBaby);
     	return newBaby;
     }; 
-    this.Mammal.prototype.toString=function toString(){ 
+    Mammal.prototype.toString=function toString(){ 
     	return '[Mammal "'+this.name+'"]';
     }; 
     
-    this.Cat.prototype = new this.Mammal();        // Here's where the inheritance occurs 
-    this.Cat.prototype.constructor=Cat;       // Otherwise instances of Cat would have a constructor of Mammal 
+    Cat.prototype = new Mammal();        // Here's where the inheritance occurs 
+    Cat.prototype.constructor=Cat;       // Otherwise instances of Cat would have a constructor of Mammal 
   
     function Cat(name){ 
       this.name=name;
     } 
     
-    this.Cat.prototype.toString=function toString(){ 
+    Cat.prototype.toString=function toString(){ 
       return '[Cat "'+this.name+'"]';
     };
     
