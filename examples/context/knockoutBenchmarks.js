cfc653fcd203c9424cea4addcd0c54da1a7c947d
Adam Moon <itchcork@gmail.com>
2013-07-23 12:50:19 -0500

    Update and rename knockout.js to knockoutBenchmarks.js

--- examples/context/knockoutBenchmarks.js
+++ examples/context/knockoutBenchmarks.js
@@ -13,7 +13,7 @@ define('knockoutBenchmarks', function () {
             KOUpdates2(KOUpdates2() + 1);
         });
         
-        var KOviewmodel = {data: self.KOData, updates1: KOUpdates1, updates2: KOUpdates2, updates3: KOUpdates3, updates4: KOUpdates4, updates5: KOUpdates5, updates6: KOUpdates6, updates7: KOUpdates7, updates8: KOUpdates8, updates9: KOUpdates9, updates10: KOUpdates10};
+        var KOviewmodel = {data: self.KOData, updates1: KOUpdates1, updates2: KOUpdates2};
 
         self.KOclear = function () {
             self.KOData("");
