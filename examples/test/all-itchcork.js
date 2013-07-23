c0f43a337cf5efba162f7884bcaf390d1af0a05a
Adam Moon <itchcork@gmail.com>
2013-07-23 12:48:52 -0500

    Update all-itchcork.js

--- examples/test/all-itchcork.js
+++ examples/test/all-itchcork.js
@@ -93,28 +93,15 @@ require(['datetime', 'ItchCork'], function (c, ic) {
     window.suiteView.add(suite);
 });
 
-require(['knockoutVSangular', 'ItchCork'], function (c, ic) {
-    var suite = new ic.Suite('knockout VS angular', c);
-    suite.add(10, function (c) {
-            c.ANGclear();
-            for (var i = 0; i < 10; i++) {
-                c.ANGpush();
-            }
-            return c.ang_scope.data.length;
-        })
+require(['knockoutBenchmarks', 'ItchCork'], function (c, ic) {
+    var suite = new ic.Suite('Knockout JS Benchmarks', c);
+    suite
         .add(10, function (c) {
             c.KOclear();
-            for (var i = 0; i < 10; i++) {
+            for (var i = 0; i < 10; i++) { 
                 c.KOpush();
             }
             return c.KOData().length;
-        })
-        .add(100, function (c) {
-            c.ANGclear();
-            for (var i = 0; i < 100; i++) {
-                c.ANGpush();
-            }
-            return c.ang_scope.data.length;
         })
         .add(100, function (c) {
             c.KOclear();
@@ -124,25 +111,11 @@ require(['knockoutVSangular', 'ItchCork'], function (c, ic) {
             return c.KOData().length;
         })
         .add(500, function (c) {
-            c.ANGclear();
-            for (var i = 0; i < 500; i++) {
-                c.ANGpush();
-            }
-            return c.ang_scope.data.length;
-        })
-        .add(500, function (c) {
             c.KOclear();
             for (var i = 0; i < 500; i++) {
                 c.KOpush();
             }
             return c.KOData().length;
-        })
-        .add(1000, function (c) {
-            c.ANGclear();
-            for (var i = 0; i < 1000; i++) {
-                c.ANGpush();
-            }
-            return c.ang_scope.data.length;
         })
         .add(1000, function (c) {
             c.KOclear();
