f852aad86e39305dd90826093865a6c632f02aed
Adam Moon <adamjmoon@gmail.com>
2013-07-24 09:02:21 -0500

    fix

--- examples/test/itchcork/inheritanceHelper.js
+++ examples/test/itchcork/inheritanceHelper.js
@@ -2,7 +2,7 @@ require(['inheritanceHelper', 'ItchCork'], function (c, ic) {
       var suite = new ic.Suite('Prototypal JS Inheritance with Helper InheritFrom Method', c);
       
       suite
-      .test(function(c) {return c.felix.toString();}).shouldBe("felix")
+      .it(function(c) {return c.felix.toString();}).shouldBe("felix")
       .benchmark();
        
        window.suiteView.add(suite);
