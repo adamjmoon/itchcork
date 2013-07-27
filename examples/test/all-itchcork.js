529c2456b8dfcd2fc52bbc4006e13b66b5d55f0a
Adam Moon <adamjmoon@gmail.com>
2013-07-27 13:38:32 -0500

    fix

--- examples/test/all-itchcork.js
+++ examples/test/all-itchcork.js
@@ -81,8 +81,7 @@ require(['primitivetypes', 'ItchCork'], function (c, ic) {
 require(['array','ItchCork'], function (c, ic) {
    var suite = new ic.Suite('Fastest method of convert array-like to actual arrays', c);
    suite.shouldEqual(1)
-       .compareBenchmarks()
-       .addToView();
+        .compareBenchmarks();
 
 });
 
--- examples/test/itchcork/array.js
+++ examples/test/itchcork/array.js
@@ -1,7 +1,6 @@
 require(['array','ItchCork'], function (c, ic) {
    var suite = new ic.Suite('Fastest method of convert array-like to actual arrays', c);
    suite.shouldEqual(1)
-       .compareBenchmarks()
-       .addToView();
+        .compareBenchmarks();
 
 });
