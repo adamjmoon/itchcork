af59acc48d780f89b8c3bbb3aaa9c4b16294b143
Adam Moon <amoon@onetechnologies.net>
2013-07-29 11:13:56 -0500

    fix

--- examples/context/inheritance.js
+++ examples/context/inheritance.js
@@ -1,21 +1,21 @@
 define("inheritance", function(){
   function context(){
   
-    this.Mammal = function Mammal(name){ 
+    function Mammal(name){
       this.name=name;
       this.offspring=[];
     };
-    this.Mammal.prototype.haveABaby=function haveABaby(){
+    Mammal.prototype.haveABaby=function haveABaby(){
       var newBaby=new this.constructor("Baby "+this.name);
       this.offspring.push(newBaby);
     	return newBaby;
     }; 
-    this.Mammal.prototype.toString=function toString(){
+    Mammal.prototype.toString=function toString(){
     	return '[Mammal "'+this.name+'"]';
     }; 
     
-    Cat.prototype = new this.Mammal();        // Here's where the inheritance occurs
-    Cat.prototype.constructor=this.Cat;       // Otherwise instances of Cat would have a constructor of Mammal
+    Cat.prototype = Mammal.prototype;        // Here's where the inheritance occurs
+    Cat.prototype.constructor=Cat;       // Otherwise instances of Cat would have a constructor of Mammal
   
     function Cat(name){
       this.name=name;
@@ -26,6 +26,7 @@ define("inheritance", function(){
     };
 
     this.Cat = Cat;
+    this.Mammal = Mammal;
     this.someAnimal = new this.Mammal('Mr. Biggles');
     this.myPet = new this.Cat('Felix');
   }
