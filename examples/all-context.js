bd55910370387128d7d1855be8cebe5807d72284
Adam Moon <amoon@onetechnologies.net>
2013-07-29 11:47:21 -0500

    fix

--- examples/all-context.js
+++ examples/all-context.js
@@ -133,40 +133,42 @@ define('datetime',function() {
     return context;
 });
 
-define("inheritance", function(){
-  function context(){
-  
-    function Mammal(name){
-      this.name=name;
-      this.offspring=[];
-    };
-    Mammal.prototype.haveABaby=function haveABaby(){
-      var newBaby=new this.constructor("Baby "+this.name);
-      this.offspring.push(newBaby);
-    	return newBaby;
-    }; 
-    Mammal.prototype.toString=function toString(){
-    	return '[Mammal "'+this.name+'"]';
-    }; 
-    
-    Cat.prototype = Mammal.prototype;        // Here's where the inheritance occurs
-    Cat.prototype.constructor=Cat;       // Otherwise instances of Cat would have a constructor of Mammal
-  
-    function Cat(name){
-      this.name=name;
-    } 
-    
-    Cat.prototype.toString=function toString(){
-      return '[Cat "'+this.name+'"]';
-    };
+define("inheritance", function () {
+    function context() {
 
-    this.Cat = Cat;
-    this.Mammal = Mammal;
-    this.someAnimal = new this.Mammal('Mr. Biggles');
-    this.myPet = new this.Cat('Felix');
-  }
-  
-  return context;
+        function Mammal(name) {
+            this.name = name;
+            this.offspring = [];
+        }
+
+        Mammal.prototype.haveABaby = function () {
+            var newBaby = new Mammal("Baby " + this.name);
+            this.offspring.push(newBaby);
+            return newBaby;
+        }
+        Mammal.prototype.toString = function () {
+            return '[Mammal "' + this.name + '"]';
+        }
+
+
+        Cat.prototype = new Mammal();        // Here's where the inheritance occurs
+        Cat.prototype.constructor = Cat;       // Otherwise instances of Cat would have a constructor of Mammal
+        function Cat(name) {
+            this.name = name;
+        }
+
+        Cat.prototype.toString = function () {
+            return '[Cat "' + this.name + '"]';
+        }
+
+
+        this.Cat = Cat;
+        this.Mammal = Mammal;
+        this.someAnimal = new Mammal('Mr. Biggles');
+        this.myPet = new Cat('Felix');
+    }
+
+    return context;
 });
 
 define('inheritanceHelper',function(){
