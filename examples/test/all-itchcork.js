2b9894080cc30138af620a3ef16872769794845d
Adam Moon <amoon@onetechnologies.net>
2013-07-29 11:10:26 -0500

    fix

--- examples/test/all-itchcork.js
+++ examples/test/all-itchcork.js
@@ -88,7 +88,7 @@ require(['inheritance', 'ItchCork'], function (c, ic) {
       .it(function(c) {return c.someAnimal instanceof c.Mammal;}).shouldBe(true)
       .it(function(c) {return c.myPet.toString();}).shouldBe('[Cat "Felix"]')
       .it(function(c) {return c.myPet instanceof c.Mammal;}).shouldBe(true)
-      .it(function(c) {return c.myPet instanceof c.Cat}).shouldBe(true)
+      .it(function(c) {return c.myPet instanceof c.Cat;}).shouldBe(true)
       .it(function(c) {c.myPet.haveABaby(); return c.myPet.offspring[0].toString();}).shouldBe('[Cat "Baby Felix"]')
 
  });
