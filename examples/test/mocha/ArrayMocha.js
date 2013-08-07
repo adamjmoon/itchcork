9fdae0fbebf254223198b2d72ab942cb14eeb13f
Adam Moon <adamjmoon@gmail.com>
2013-08-06 21:19:59 -0500

    fix

--- examples/test/mocha/ArrayMocha.js
+++ examples/test/mocha/ArrayMocha.js
@@ -2,10 +2,10 @@ require(['arrayMocha'], function (c) {
     var context = new c();
     describe('arrayMocha', function () {
         describe('#indexOf()', function () {
-            it('should return -1 when the value is not present ', function (context) {
+            it('should return -1 when the value is not present ', function () {
                 return context.arr.indexOf(5).should.equal(-1);
             });
-            it('should return 0 valued index when the value is present', function (context) {
+            it('should return 0 valued index when the value is present', function () {
                 return context.arr.indexOf(1).should.equal(0);
             });
 
