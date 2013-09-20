8c7f7fb1fa7de76795a1b1fe0518335708934e94
Adam Moon <adamjmoon@gmail.com>
2013-09-20 01:01:48 -0500

    fix

--- vendor/itchcork.js
+++ vendor/itchcork.js
@@ -1136,91 +1136,83 @@ define("itchcork", ['Suite', 'Test', 'Spy', 'Verify'], function (Suite, Test, Sp
             $("#topNav").show();
             $('div.frame').show();
 
-                    var postResults = function (stats, callback) {
 
-                        $.post("/stats", { stats: stats}, function () {
-                            if (callback) {
-                                callback();
+            var runItchCork = function () {
+                "use strict";
+                $.get('/benchmarkList', function (benchmarks) {
+                    require(benchmarks, function () {
+                    });
+                });
+            };
+
+            var runMocha = function () {
+                chai.use(sinonChai);
+                window.assert = chai.assert;
+                window.should = chai.should();
+                   require(['chai', 'sinon-chai','mocha'], function (chai, sinonChai, mocha) {
+
+                        if (window.location.search) {
+                            var array = window.location.search.split('?');
+                            var spec = array[1];
+                            run([spec]);
+                        } else {
+                            $.get('/specs', function (specs) {
+
+                                run(specs);
+
+                            });
+                        }
+
+
+                        var run = function (specs) {
+                            if (window.mochaPhantomJS) {
+                                mochaPhantomJS.run();
                             }
-                        });
-                    };
+                            else {
+                                mocha.checkLeaks();
+                                mocha.globals(['jQuery']);
+                                mocha.run();
+                                mocha.setup('bdd');
+                                mocha.reporter('html');
+                                require(specs, function () {
+                                    var runner = mocha.run();
 
-                    var runItchCork = function () {
-                        "use strict";
-                        $.get('/benchmarkList', function (benchmarks) {
-                            require(benchmarks, function () {
-                            });
-                        });
-                    };
-
-                    var runMocha = function () {
-                        chai.use(sinonChai);
-                        window.assert = chai.assert;
-                        window.should = chai.should();
-                           require(['chai', 'sinon-chai','mocha'], function (chai, sinonChai, mocha) {
-
-                                if (window.location.search) {
-                                    var array = window.location.search.split('?');
-                                    var spec = array[1];
-                                    run([spec]);
-                                } else {
-                                    $.get('/specs', function (specs) {
-
-                                        run(specs);
-
-                                    });
-                                }
-
-
-                                var run = function (specs) {
-                                    if (window.mochaPhantomJS) {
-                                        mochaPhantomJS.run();
-                                    }
-                                    else {
-                                        mocha.checkLeaks();
-                                        mocha.globals(['jQuery']);
-                                        mocha.run();
-                                        mocha.setup('bdd');
-                                        mocha.reporter('html');
-                                        require(specs, function () {
-                                            var runner = mocha.run();
-
-                                            runner.on('end', function () {
-                                                window.suiteView.totalTests(runner.total);
-                                                window.suiteView.totalPassed(runner.total - runner.failures);
-                                                window.suiteView.totalFailed(runner.failures);
-                                                    _.each(runner.suite.suites,
-                                                        function (s) {
-                                                            require([s.title], function (c) {
-                                                               // var suite = new window.ItchCork.Suite(s.title, c, "mocha");
-                                                            });
-                                                        });
-                                                var suites = $("ul#mocha-report li.suite ul");
-                                                $("#collapse").click(function () {
-                                                    $(suites).each(function (index, element) {
-                                                        element.hidden = true;
+                                    runner.on('end', function () {
+                                        window.suiteView.totalTests(runner.total);
+                                        window.suiteView.totalPassed(runner.total - runner.failures);
+                                        window.suiteView.totalFailed(runner.failures);
+                                            _.each(runner.suite.suites,
+                                                function (s) {
+                                                    require([s.title], function (c) {
+                                                       // var suite = new window.ItchCork.Suite(s.title, c, "mocha");
                                                     });
-                                                    $("#collapse").hide();
-                                                    $("#expand").show();
                                                 });
-                                                $("#expand").click(function () {
-                                                    $(suites).each(function (index, element) {
-                                                        element.hidden = false;
-                                                    });
-                                                    $("#expand").hide();
-
-                                                    $("#collapse").show();
-                                                });
-                                                postResults(runner.stats, function () {
-                                                    }
-                                                );
-                                                window.suiteView.bindView();
+                                        var suites = $("ul#mocha-report li.suite ul");
+                                        $("#collapse").click(function () {
+                                            $(suites).each(function (index, element) {
+                                                element.hidden = true;
                                             });
+                                            $("#collapse").hide();
+                                            $("#expand").show();
                                         });
-                                    }
-                                }
-                            });
-                        };
+                                        $("#expand").click(function () {
+                                            $(suites).each(function (index, element) {
+                                                element.hidden = false;
+                                            });
+                                            $("#expand").hide();
+
+                                            $("#collapse").show();
+                                        });
+                                        postResults(runner.stats, function () {
+                                            }
+                                        );
+                                        window.suiteView.bindView();
+                                    });
+                                });
+                            }
+                        }
+                    });
+                };
 
 
                     view = window.location.pathname;
