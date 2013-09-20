f54400071c94b838207fd9d9dc83421a91ba4a77
Adam Moon <adamjmoon@gmail.com>
2013-09-20 01:16:12 -0500

    fix

--- vendor/itchcork.js
+++ vendor/itchcork.js
@@ -1126,41 +1126,38 @@ define("itchcork", ['Suite', 'Test', 'Spy', 'Verify'], function (Suite, Test, Sp
     window.ItchCork = new itchcork();
     return window.ItchCork;
 });
+require(['SuiteView', 'itchcork', 'context'], function (sv, itchcork) {
+
+    window.suiteView = new sv();
+    var context = '';
+    if (window.location.pathname && window.location.pathname.length > 1)
+        context = window.location.pathname.split('/')[1];
+    else if (window.location.hash && window.location.hash.length > 1)
+        context = window.location.hash.split('#')[1];
+
+    var suite = context != '' ? window.suiteView.unitTestFrameworkManager.getFramework() + '/' + context : 'all-' + window.suiteView.unitTestFrameworkManager.getFramework();
+    var suiteFilePath = suiteView.contextRoot() + 'examples/test';
+
+    requirejs.config({
+        baseUrl: 'https://',
+        paths: {
+            'context': suiteView.contextRoot() + 'examples/all-context',
+            'suite': suiteFilePath + "/" + suite,
+            'suitePath': suiteFilePath
+        }
+    });
+
+    $("#topNav").show();
+    $('div.frame').show();
 
 
-    require(['SuiteView', 'itchcork', 'context'], function (sv, itchcork) {
+    if (window.suiteView.unitTestFrameworkManager.init() === "itchcork") {
+        require(['suite'], function () {
 
-        window.suiteView = new sv();
-        var context = '';
-        if (window.location.pathname && window.location.pathname.length > 1)
-            context = window.location.pathname.split('/')[1];
-        else if (window.location.hash && window.location.hash.length > 1)
-            context = window.location.hash.split('#')[1];
-
-        var suite = context != '' ? window.suiteView.unitTestFrameworkManager.getFramework() + '/' + context : 'all-' + window.suiteView.unitTestFrameworkManager.getFramework();
-        var suiteFilePath = suiteView.contextRoot() + 'examples/test';
-
-        requirejs.config({
-            baseUrl: 'https://',
-            paths: {
-                'context': suiteView.contextRoot() + 'examples/all-context',
-                'suite': suiteFilePath + "/" + suite,
-                'suitePath': suiteFilePath
-            }
         });
+    }
 
-            $("#topNav").show();
-            $('div.frame').show();
-
-
-            if (window.suiteView.unitTestFrameworkManager.init() === "itchcork") {
-                require(['suite'], function () {
-
-                });
-            }
-
-                    });
-                };
+});
 
 
 
